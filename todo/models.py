557885b7368036fedc070f1164676cdec1cc0982
jolenewai <jolene.wai@gmail.com>
2020-05-04 07:32:08 +0000

    created read route for todo

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+# This class represents the Todo table in the database
+# Todo will inherit modesls.Model, the latter is a Django class
+# that already has the functionality we need. By inheriting, Todo will 
+# have all those functionalities
+class Todo(models.Model):
+    name = models.CharField(max_length=255, blank=False)
+    done = models.BooleanField(blank=False, default=False)
+
+    def __str__(self):
+        return self.name
--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render, HttpResponse
+from .models import Todo
 
 # Create your views here.
 def index(request):
-    return render(request, 'todo/index.template.html')
+
+    #to fetch all results in Todo
+    # this will return cursor, similar to db['collection'].find()
+    all_todos = Todo.objects.all() 
+
+    return render(request, 'todo/index.template.html', {
+        'all_todos':all_todos
+    })
+
